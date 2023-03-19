925f54848b48292998077651486547b2cd24529f
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2023-03-19 15:33:22 -0400

    trying to call internal folder

--- tower/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Shahbaz-mahmood123/GoScripts/tower
-
-go 1.19
--- tower/tower.go
+++ tower/tower.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func tower() {
+func GetComputeEnv() {
 
 	url := "https://sm-k8s.dev-tower.net/api/compute-envs?workspaceId=251085962711837"
 	token := "eyJ0aWQiOiA2fS5kNTQ1MDBkNzc4MGM4OWQ0YmYzYWFjYTQ3NTIwZGExN2EyNTAyZDAw"
