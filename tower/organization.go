27275cb832c970a63e661bd4eb28e9b41559893c
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2023-03-21 07:53:03 -0400

    changes to http client and more tower GET functions

--- tower/organization.go
+++ tower/organization.go
@@ -1,12 +1,11 @@
 package tower
 
-func GetServiceInfo() string {
-	uri := "service-info"
+func GetOrganizations() string {
+	uri := "orgs"
 
 	response, err := httpClient(uri)
 	if err != nil {
 		panic(err)
 	}
 	return response
-
 }
--- tower/towerClient.go
+++ tower/towerClient.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-func httpClient(uri string) string {
+func httpClient(uri string) (string, error) {
 
 	url := "https://sm-k8s.dev-tower.net/api/" + uri
 	token := "eyJ0aWQiOiA2fS5kNTQ1MDBkNzc4MGM4OWQ0YmYzYWFjYTQ3NTIwZGExN2EyNTAyZDAw"
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	req.Header.Set("Authorization", "Bearer "+token)
@@ -20,15 +20,15 @@ func httpClient(uri string) string {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	return string(body)
+	return string(body), nil
 }
